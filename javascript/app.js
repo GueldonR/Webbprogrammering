c18e16e45f6d1cd1bc2f5ff9a66af972c5d77f9e
gueldon <149371777+GueldonR@users.noreply.github.com>
2025-11-05 11:26:23 +0100

    Added a functional sidebar

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+document.querySelector(".closebtn").addEventListener("click", closeNav);
+document.querySelector("button").addEventListener("click", openNav);
