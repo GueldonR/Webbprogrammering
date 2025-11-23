2c919b29fea89f29f6ca41c3bf0c3f49ee144b0a
gueldon <149371777+GueldonR@users.noreply.github.com>
2025-11-23 19:44:04 +0100

    added navigation history logic

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -6,3 +6,30 @@ function showpage(pageid) {
   }
   document.getElementById(pageid).style.display = "block";
 }
+
+function showDialog(token) {
+  alert("showing dialog: " + token);
+
+  if (localStorage.getItem("flag") === "true") {
+    return;
+  }
+  // We need an if-statement and a way to avoid saving history when called from historyChange
+  // Otherwise we end up in an endless loop returning to the second to last page
+
+  history.pushState(token, "Titel: " + token, "");
+}
+
+function historyChange(event) {
+  localStorage.setItem("flag", "true");
+  alert("token changed to: " + event.state);
+  showDialog(event.state);
+  localStorage.removeItem("flag");
+  console.log("flag removed");
+}
+
+function setupHistory() {
+  document.getElementById("outputValue").value = history.state;
+  window.onpopstate = function (event) {
+    historyChange(event);
+  };
+}
