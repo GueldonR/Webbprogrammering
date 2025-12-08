014b4fd006598a824206938a7b0bd60c5410be0a
gueldon <149371777+GueldonR@users.noreply.github.com>
2025-12-08 10:33:00 +0100

    added game logic; hooked up game.js to html page

--- javascript/game.js
+++ javascript/game.js
@@ -1,26 +1,70 @@
-var ctx;
-var x = -200;
-
 document.addEventListener("DOMContentLoaded", function () {
-  startcanvas();
+  document.getElementById("playButton").addEventListener("click", function () {
+    startGame();
+  });
 });
 
-function startcanvas() {
-  var c = document.getElementById("canvasid");
+var ctx;
+var c;
+var x = 0;
+var objects = [];
+
+// init av spelet
+function startGame() {
+  c = document.getElementById("canvasid");
   ctx = c.getContext("2d");
 
+  objects.push(new componentFactory(30, 30, "red", 10, 120, 1, 0));
+
   redraw();
+
+  // hur ofta vi kör spelloopen
+  window.setInterval(gameLoop, 30);
 }
 
+// händelseförloppet
+function gameLoop() {
+  // timerfunktion
+
+  for (obj in objects) {
+    obj.update();
+  }
+}
+
+// Redraw screen
 function redraw() {
-  ctx.clearRect(0, 0, 100, 100);
+  ctx.clearRect(0, 0, c.width, c.height);
 
-  x++;
-
-  ctx.beginPath();
-  ctx.moveTo(50, 20);
-  ctx.lineTo(x, 100);
-  ctx.stroke();
-
+  for (obj in objects) {
+    obj.frame();
+  }
   window.requestAnimationFrame(redraw);
 }
+
+// klass för att skapa komponenter
+class componentFactory {
+  constructor(width, height, color, x, y, speedX, speedY) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+
+    this.speedX = speedX;
+    this.speedY = speedY;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  frame() {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rectangle(0, 0, this.width, this.height);
+    ctx.restore();
+  }
+}
+
+// 3 objekt: bakgrund, sprite(spelare), pipe
