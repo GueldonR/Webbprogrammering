90ff1fa6f652f249d5ed98f5af9c21742b9fba8d
gueldon <149371777+GueldonR@users.noreply.github.com>
2025-12-08 12:51:10 +0100

    added some logic and assets

--- javascript/game.js
+++ javascript/game.js
@@ -1,20 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("playButton").addEventListener("click", function () {
-    startGame();
-  });
+window.addEventListener("load", function () {
+  class Game {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+    }
+    update() {}
+    draw() {}
+  }
+
+  class Player {
+    constructor(game) {
+      this.game = game;
+      this.width = 100;
+      this.height = 91.3;
+      this.x = 0;
+      this.y = 100;
+    }
+    update() {}
+    draw(context) {
+      context.fillrect(this.x, this.y, this.width, this.height);
+    }
+  }
+  game = new Game();
+  console.log(game);
 });
-
-class Game {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-  }
-  update() {}
-  draw() {}
-}
-
-class Player {
-  constructor(game) {
-    this.game = game;
-  }
-}
