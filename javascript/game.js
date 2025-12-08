1f040a7272cd9161b1bf037417d475c83dd318ee
gueldon <149371777+GueldonR@users.noreply.github.com>
2025-12-08 11:28:50 +0100

    update index.html to include game.css and refactor game.js

--- javascript/game.js
+++ javascript/game.js
@@ -4,67 +4,17 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-var ctx;
-var c;
-var x = 0;
-var objects = [];
-
-// init av spelet
-function startGame() {
-  c = document.getElementById("canvasid");
-  ctx = c.getContext("2d");
-
-  objects.push(new componentFactory(30, 30, "red", 10, 120, 1, 0));
-
-  redraw();
-
-  // hur ofta vi kör spelloopen
-  window.setInterval(gameLoop, 30);
-}
-
-// händelseförloppet
-function gameLoop() {
-  // timerfunktion
-
-  for (obj in objects) {
-    obj.update();
-  }
-}
-
-// Redraw screen
-function redraw() {
-  ctx.clearRect(0, 0, c.width, c.height);
-
-  for (obj in objects) {
-    obj.frame();
-  }
-  window.requestAnimationFrame(redraw);
-}
-
-// klass för att skapa komponenter
-class componentFactory {
-  constructor(width, height, color, x, y, speedX, speedY) {
+class Game {
+  constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.x = x;
-    this.y = y;
-    this.color = color;
-
-    this.speedX = speedX;
-    this.speedY = speedY;
   }
-
-  update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-  }
-
-  frame() {
-    ctx.save();
-    ctx.translate(this.x, this.y);
-    ctx.rectangle(0, 0, this.width, this.height);
-    ctx.restore();
-  }
+  update() {}
+  draw() {}
 }
 
-// 3 objekt: bakgrund, sprite(spelare), pipe
+class Player {
+  constructor(game) {
+    this.game = game;
+  }
+}
